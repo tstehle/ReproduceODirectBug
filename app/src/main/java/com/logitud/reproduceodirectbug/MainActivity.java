f53878498b4c8fe74feddc9f8a1f3beca19fb138
Thomas <t-stehle@logitud.fr>
2015-02-24 16:55:27 +0100

    Run test multiple times to prevent false positives when the buffers are aligned by chance.

--- app/src/main/java/com/logitud/reproduceodirectbug/MainActivity.java
+++ app/src/main/java/com/logitud/reproduceodirectbug/MainActivity.java
@@ -1,8 +1,14 @@
 package com.logitud.reproduceodirectbug;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -12,9 +18,7 @@ import java.io.File;
 
 public class MainActivity extends ActionBarActivity {
 
-    private static final String TAG = "MainActivity";
-
-    // SD
+    // UI
     TextView tv;
 
 
@@ -23,38 +27,61 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // MISC
+        Spannable logMessage = null;
+        ForegroundColorSpan successSpanColor = new ForegroundColorSpan(getResources().getColor(R.color.DarkGreen));
+        ForegroundColorSpan errorSpanColor = new ForegroundColorSpan(getResources().getColor(R.color.DarkRed));
+
         // UI
         String absolutePathOfExternalSD = getAbsolutePathOfExternalSD();
         tv = (TextView) findViewById(R.id.tv);
-        tv.append("\nWe think that the path of external sd card is:\n  " + absolutePathOfExternalSD + "\nPlease make sure it is correct.\n");
+        tv.setText("\n");
+
+        // Log path
+        tv.append("\nWe think that the path of the app directory on the external SdCard is:\n");
+        logMessage = new SpannableString(absolutePathOfExternalSD);
+        StyleSpan boldSpan = new StyleSpan( Typeface.BOLD );
+        logMessage.setSpan(boldSpan, 0, logMessage.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv.append(logMessage);
+        tv.append("\n");
 
         // JNI
         NDKFileAccessTest ndkFileAccessTest = new NDKFileAccessTest();
 
+        // First test, without O_DIRECT
         char res = ndkFileAccessTest.writeToFileWithoutODirect(absolutePathOfExternalSD + "/testFile.txt");
-        Log.w(TAG, Integer.toHexString(res));
         if (res == 0) {
-            tv.append("\n1.) Write to SDCard without O_DIRECT fails... NOT NORMAL AT ALL, this should never happen!\n");
+            logMessage = new SpannableString("\n1.) Write to SDCard without O_DIRECT fails... NOT NORMAL AT ALL, this should never happen!\n");
+            logMessage.setSpan(errorSpanColor, 0, logMessage.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+            tv.append(logMessage);
         } else {
-            tv.append("\n1.) Write to SDCard without O_DIRECT succeeds, that's normal.\n");
+            logMessage = new SpannableString("\n1.) Write to SDCard without O_DIRECT succeeds, that's normal.\n");
+            logMessage.setSpan(successSpanColor, 0, logMessage.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+            tv.append(logMessage);
         }
 
-        char res2 = ndkFileAccessTest.writeToFileWithODirect(absolutePathOfExternalSD + "/testFile.txt");
-        Log.w(TAG, Integer.toHexString(res2));
-        if (res2 == 0) {
-            tv.append("\n2.) Write to SDCard with O_DIRECT fails... the O_DIRECT bug is not fixed.\n");
-        } else {
-            tv.append("\n2.) Write to SDCard with O_DIRECT succeeds! No Bug!\n");
+        // Second test with O_DIRECT.
+        // Run multiple times to avoid false positives (when the buffers are aligned by chance)
+        int RUN_X_TIMES = 10;
+        char res2 = 0;
+        for (int i = 0; i < RUN_X_TIMES; i++) {
+            res2 += ndkFileAccessTest.writeToFileWithODirect(absolutePathOfExternalSD + "/testFile.txt");
         }
 
-        //ndkFileAccessTest.writeToFileWithODirect2(absolutePathOfExternalSD + "/writeToFileAtPathWithODirect.txt");
-
-
+        if (res2 < RUN_X_TIMES) {
+            logMessage = new SpannableString("\n2.) Write to SDCard with O_DIRECT fails... the O_DIRECT bug is NOT fixed.\n");
+            logMessage.setSpan(errorSpanColor, 0, logMessage.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+            tv.append(logMessage);
+        } else {
+            logMessage = new SpannableString("\n2.) Write to SDCard with O_DIRECT succeeds! No Bug!\n");
+            logMessage.setSpan(successSpanColor, 0, logMessage.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+            tv.append(logMessage);
+        }
     }
 
 
     ///////////
-    // Access
+    // Helpers
 
     private String getAbsolutePathOfExternalSD() {
         String cardPath = "";
@@ -76,7 +103,7 @@ public class MainActivity extends ActionBarActivity {
 
         return cardPath;
     }
-    // END Access
+    // END Helpers
     ////////////
 
 
